626fa43b711eb4e34967d0dea9bdfa96c0d4b2de
rsguayax <rsguayax@gmail.com>
2020-02-07 09:06:55 -0500

    cambios 2

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,22 @@
 public class Test1 {
+
+    int a;
+    private int b;
+
+    public Test1(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Test1(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 }
